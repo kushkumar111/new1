f02755e935c6e8a4fb7f69981f5891e88892d21f
kushkumar111 <76873779+kushkumar111@users.noreply.github.com>
2021-11-27 14:59:36 +0530

    Create task_1.js

---
+++ task_1.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TASK 1</title>
+    <script src="task 1.js"></script>
+</head>
+<body>
+    <center>
+    <h1>Java Script CS3403 assignment</h1><hr><br>
+    <h2>Open the Console to view details of three person </2>
+    </center>
+</body>
+</html>
